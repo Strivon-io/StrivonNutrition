b6fbd378c643f889185262088af57dc925b6f7f1
HuguesRomain <huguesromainpro@gmail.com>
2023-11-25 02:11:42 +0100

    remove comments

--- src/components/molecules/BottomSheet/index.tsx
+++ src/components/molecules/BottomSheet/index.tsx
@@ -23,10 +23,8 @@ import { Backdrop } from './Backdrop'
 import { spacing } from '@constants/theme'
 import { isSmallScreen } from '@utils/deviceDetector'
 
-// Get the screen height
 const { height: SCREEN_HEIGHT } = Dimensions.get('window')
 
-// Define the props for the BottomSheet component
 type bottomSheetProps = {
   children?: React.ReactNode
   maxHeight?: number
@@ -40,21 +38,16 @@ export type bottomSheetRef = {
   close: () => void
 }
 
-// Create the BottomSheet component
 export const MainBottomSheet = React.forwardRef<
   bottomSheetRef,
   bottomSheetProps
 >(({ children, style, maxHeight = SCREEN_HEIGHT, onClose }, ref) => {
-  // Create a shared value for translateY animation
   const translateY = useSharedValue(maxHeight)
 
-  // Define the maximum translateY value for the BottomSheet
   const MAX_TRANSLATE_Y = -maxHeight
 
-  // Create a shared value to track the active state
   const active = useSharedValue(false)
 
-  // Function to scroll to a specific Y position
   const scrollTo = useCallback((destination: number) => {
     'worklet'
     active.value = destination !== maxHeight
@@ -62,16 +55,13 @@ export const MainBottomSheet = React.forwardRef<
     translateY.value = withSpring(destination, {
       mass: 0.4,
     })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  // Function to close the BottomSheet
   const close = useCallback(() => {
     'worklet'
     return scrollTo(maxHeight)
   }, [maxHeight, scrollTo])
 
-  // Expose functions and values through useImperativeHandle
   useImperativeHandle(
     ref,
     () => ({
@@ -87,29 +77,23 @@ export const MainBottomSheet = React.forwardRef<
     [close, scrollTo, active.value],
   )
 
-  // Create a shared value for context
   const context = useSharedValue({ y: 0 })
 
-  // Create a gesture handler for pan gestures
   const gesture = Gesture.Pan()
     .onStart(() => {
       context.value = { y: translateY.value }
     })
     .onUpdate((event) => {
-      // Handle just gestures to swipe down
       if (event.translationY > -50) {
-        // Update the translateY value with clamping
         translateY.value = event.translationY + context.value.y
       }
     })
     .onEnd((event) => {
       if (event.translationY > 100) {
-        // Close the Action Tray when the user swipes down
         if (onClose) {
           runOnJS(onClose)()
         } else close()
       } else {
-        // Restore to the previous position if the users doesn't swipe down enough
         scrollTo(context.value.y)
       }
     })
@@ -118,12 +102,10 @@ export const MainBottomSheet = React.forwardRef<
     setKeyboardOpen(true)
   }
 
-  // Function to handle keyboard close event
   const handleKeyboardClose = () => {
     setKeyboardOpen(false)
   }
 
-  // Subscribe to keyboard events
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener(
       'keyboardDidShow',
@@ -134,14 +116,12 @@ export const MainBottomSheet = React.forwardRef<
       handleKeyboardClose,
     )
 
-    // Clean up listeners
     return () => {
       keyboardDidShowListener.remove()
       keyboardDidHideListener.remove()
     }
   }, [])
 
-  // Update the bottomSheetContainer style
   const rbottomSheetStyle = useAnimatedStyle(() => {
     const borderRadius = interpolate(
       translateY.value,
@@ -167,9 +147,7 @@ export const MainBottomSheet = React.forwardRef<
 
   return (
     <>
-      {/* Backdrop to handle tap events */}
       <Backdrop onTap={onClose ?? close} isActive={active} />
-      {/* Gesture detector to handle pan gestures */}
       <GestureDetector gesture={gesture}>
         <Animated.View
           style={[styles.bottomSheetContainer, rbottomSheetStyle, style]}
@@ -183,7 +161,6 @@ export const MainBottomSheet = React.forwardRef<
   )
 })
 
-// Define the styles for the BottomSheet component
 const styles = StyleSheet.create({
   bottomSheetContainer: {
     backgroundColor: '#FFF',
