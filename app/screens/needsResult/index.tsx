e1657a7eef59537890352bcacc679aae92653c3f
Hugues ROMAIN <44021745+HuguesRomain@users.noreply.github.com>
2024-05-08 12:04:26 +0200

    calories explanation page (#13)

--- app/screens/needsResult/index.tsx
+++ app/screens/needsResult/index.tsx
@@ -1,33 +1,37 @@
-import { FC } from "react";
-import { useTranslation } from "react-i18next";
-import { View, TouchableOpacity, StyleSheet } from "react-native";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-import { NavigatorParamList } from "~navigators/app-navigator";
-import { Text } from "~components/atoms/text";
-import { Layout } from "~components/layout/layout";
-import { MainButton } from "~components/molecules/mainButton";
-import { boxShadow, colors, spacing } from "~constants/theme";
+import { NavigatorParamList } from '~navigators/app-navigator'
+import { Text } from '~components/atoms/text'
+import { Layout } from '~components/layout/layout'
+import { MainButton } from '~components/molecules/mainButton'
+import { boxShadow, colors, spacing } from '~constants/theme'
 
 type NeedResultScreenProps = NativeStackScreenProps<
   NavigatorParamList,
-  "needsResult"
->;
+  'needsResult'
+>
 
 export const NeedsResultScreen: FC<NeedResultScreenProps> = ({
   navigation,
 }) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   const handleViewRecipes = () => {
-    navigation.navigate("recipesResult");
-  };
+    navigation.navigate('recipesResult')
+  }
+
+  const handleHowItWorks = () => {
+    navigation.navigate('needsResultExplanation')
+  }
 
   return (
     <Layout isHeaderLogo>
       <View style={styles.wrapper}>
         <Text fontFamily="Avenir-Medium" fontSize="l" textAlign="center">
-          {t("youNeed")}
+          {t('youNeed')}
         </Text>
         <Text
           fontFamily="Avenir-Bold-Italic"
@@ -38,31 +42,34 @@ export const NeedsResultScreen: FC<NeedResultScreenProps> = ({
           2700Kcal
         </Text>
         <Text fontFamily="Avenir-Medium" fontSize="l" textAlign="center">
-          {t("perDayToMaintainYourCurrentWeight")}
+          {t('perDayToMaintainYourCurrentWeight')}
         </Text>
-        <TouchableOpacity style={{ marginTop: spacing.xs }}>
+        <TouchableOpacity
+          style={{ marginTop: spacing.xs }}
+          onPress={handleHowItWorks}
+        >
           <Text
             fontFamily="Avenir-Medium"
             color="Alizarin"
             textDecorationLine="underline"
             textAlign="center"
           >
-            {t("howItWorks")}
+            {t('howItWorks')}
           </Text>
         </TouchableOpacity>
       </View>
 
       <MainButton
         style={boxShadow}
-        label={t("viewMyFirstRecipes")}
+        label={t('viewMyFirstRecipes')}
         onPress={handleViewRecipes}
       />
     </Layout>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
   },
-});
+})
---
+++ app/screens/needsResultExplanation/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { View } from 'react-native'
+import { Text } from '~components/atoms/text'
+import { Layout } from '~components/layout/layout'
+import { spacing } from '~constants/theme'
+
+export const NeedsResultExplanationScreen: React.FC = () => {
+  const { t } = useTranslation()
+  return (
+    <Layout pageTitle={t('howItWorks')} isBackArrow isHeader scrollView>
+      <View>
+        <Text
+          style={{ marginBottom: spacing.s }}
+          fontFamily="Avenir-Bold-Italic"
+          fontSize="l"
+        >
+          {`${t('howCaloriesCalculationWorks')} ?`}
+        </Text>
+        <Text fontSize="m">{t('howCaloriesCalculationWorksExplanation')}</Text>
+      </View>
+    </Layout>
+  )
+}
